123be57def1668bc11cb17edfdee67aaf417d6b6
root <root@mks-m75q-3.mkashi.com>
2020-12-12 00:03:34 +0900

    スナップショットの取り方を変更

--- backup_vm.sh
+++ backup_vm.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 if [ "$1" != "cron" ]; then
     echo "Please run this command from cron" >&2
@@ -9,33 +9,21 @@ SNAPSHOT_SIZE_THRESH=10737418240
 SNAPSHOT_MAX_NUM=14
 
 timestamp=$(date '+%Y%m%d%H%M%S')
+snapshot_name=${timestamp}-snap
 echo "Backup start at $(date '+%Y/%m/%d %H:%M:%S')"
 for host in mks-m75q-1 mks-m75q-2 mks-m75q-3;do
     vms=$(ssh $host virsh list --name | grep -v template)
-    snapshot_name=backup_vm-${timestamp}
     
     for vm in $vms;do
 	echo "Start $vm"
-	disks=$(ssh $host virsh domblklist $vm | tail -n +3 | egrep '\.qcow2$|\.img$')
+	disks=$(ssh $host virsh domblklist $vm | tail -n +3 | egrep '\.qcow2$|\.img|-snap$')
 	cmd="ssh $host virsh snapshot-create-as --name ${snapshot_name} --domain ${vm} --disk-only --atomic --no-metadata --quiesce"
-	cmd_opt=""
-	snapshot_deleted=""
 	while read line;do
 	    dev=$(echo "$line" | awk '{print $1}')
 	    path=$(echo "$line" | awk '{print $2}')
 	    path_base=${path%.*}
-	    path_base=${path_base%.*}
-	    path_ext=${path##*.}
-	    echo "$path" | grep /var/lib/libvirt/images/ >/dev/null 2>&1
-	    if [ $? -eq 0 ];then
-		cmd_opt=" ${cmd_opt} --diskspec ${dev},file=${path_base}.${timestamp}.${path_ext}"
-	    else
-		if [ -n "$dev" ];then
-		    cmd_opt=" ${cmd_opt} --diskspec ${dev},snapshot=no"
-		fi
-	    fi
 	    # delete old snapshots
-	    num_snaps=$(ls ${path_base}.*.* 2>/dev/null | wc -l)
+	    num_snaps=$(ls ${path_base}.* 2>/dev/null | wc -l)
 	    if [ $num_snaps -ge $SNAPSHOT_MAX_NUM ];then
 		snaps=($(ls ${path_base}.*.* 2>/dev/null))
 		for ((i=0; i<$num_snaps-1; i++));do
@@ -44,10 +32,6 @@ for host in mks-m75q-1 mks-m75q-2 mks-m75q-3;do
 			base=${snaps[$i]}
 			top=${snaps[(($i+1))]}
 			ssh $host virsh blockcommit $vm $dev --base $base --top $top --verbose --wait --delete 2>&1
-			if [ $? -eq 0 ];then
-			    tmp=${top%.*}
-			    snapshot_deleted="backup_vm-${tmp##*.}"
-			fi
 			break
 		    fi
 		done
@@ -56,20 +40,17 @@ for host in mks-m75q-1 mks-m75q-2 mks-m75q-3;do
 		fi
 	    fi
 	done < <(echo "$disks")
-	# delete snapshot
-	if [ -n "$snapshot_deleted" ];then
-	    ssh $host virsh snapshot-delete --domain $vm --metadata $snapshot_deleted
-	fi
 	# take snapshot
-	if [ -n "$cmd_opt" ];then
-	    ${cmd}${cmd_opt}
-	fi
+	${cmd}
     done
 done
+
+# reload libvirtd
+~/bin/allssh systemctl restart libvirtd
 
 # do rsync
 ops="-avz4 --delete --no-group --progress"
 rsync $ops /etc/libvirt/qemu/ rsync://mkashi@raspberrypi/backup/qemu/
-rsync $ops --exclude='*.tmp' --exclude="*.${timestamp}.*" /var/lib/libvirt/images/ rsync://mkashi@raspberrypi/backup/images/
+rsync $ops --exclude='*.tmp' --exclude="*.${snapshot_name}" /var/lib/libvirt/images/ rsync://mkashi@raspberrypi/backup/images/
 
 echo "Backup end at $(date '+%Y/%m/%d %H:%M:%S')"
