82c1cc88596b044b2a7ebc12a3bcbb2270841120
Masafumi <mkashi3@gmail.com>
2021-06-15 14:41:41 +0900

    Update

---
+++ check_glusterfs_status.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+is_leader=$(ip a | grep 192.168.151.254)
+if [ -n "$is_leader" ];then
+    is_healing=$(/root/bin/cluster status | grep -i GlusterFS | grep -v OK)
+    if [ -n "$is_healing" ];then
+	echo "GlusterFS is healing on m75s" | mail -s "GlusterFS status notice" mkashi3@gmail.com
+    fi
+fi
--- vm_enable_ha.sh
+++ vm_enable_ha.sh
@@ -5,5 +5,10 @@ if [ -z $1 ];then
     exit 2
 fi
 
+if [ ! -f /root/qemu/$1.xml ];then
+    echo "No /root/qemu/$1.xml"
+    exit 2
+fi
+
 pcs resource create vm-$1 ocf:heartbeat:VirtualDomain config=/root/qemu/$1.xml migration_transport=ssh meta allow-migrate=true
 
