711e5d4103c8d94ef7a269fd2d71db2d89a7fafe
Masafumi <mkashi3@gmail.com>
2021-06-15 14:46:51 +0900

    Update

--- check_glusterfs_status.sh
+++ check_glusterfs_status.sh
@@ -2,7 +2,7 @@
 
 is_leader=$(ip a | grep 192.168.151.254)
 if [ -n "$is_leader" ];then
-    is_healing=$(/root/bin/cluster status | grep -i GlusterFS | grep -v OK)
+    is_healing=$(/usr/local/bin/cluster status | grep -i GlusterFS | grep -v OK)
     if [ -n "$is_healing" ];then
 	echo "GlusterFS is healing on m75s" | mail -s "GlusterFS status notice" mkashi3@gmail.com
     fi
